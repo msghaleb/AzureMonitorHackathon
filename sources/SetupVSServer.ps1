a975d400ac48cea8364df409a8598e81fa20eed5
Max Melcher <max.melcher@microsoft.com>
2021-06-08 10:45:40 +0200

    Update SetupVSServer.ps1
    failed in my Images

--- sources/SetupVSServer.ps1
+++ sources/SetupVSServer.ps1
@@ -14,12 +14,16 @@ $proc = (Start-Process -FilePath $exeFileNetCore.Name.ToString() -ArgumentList (
 $proc | Wait-Process 
 
 
-# Disable Internet Explorer Enhanced Security Configuration
-$AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
-$UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
-Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0 -Force
-Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0 -Force
-Stop-Process -Name Explorer -Force
+try
+{
+    # Disable Internet Explorer Enhanced Security Configuration
+    $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+    $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
+    Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0 -Force
+    Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0 -Force
+    Stop-Process -Name Explorer -Force
+} 
+catch {}
 
 # Download eShopOnWeb to c:\eShopOnWeb and extract contents
 $zipFileeShopTemp = [System.IO.Path]::GetTempPath().ToString() + "eShopOnWeb-master.zip"
